2085c002cfa95d0f142fe887bf94f5a30ddf7767
amadeusantos <amadeusantos31@gmail.com>
2024-03-03 19:50:27 -0300

    [feat] REAMED to start projects spring-boot

--- gateway/src/main/java/com/angels/gateway/controller/PredictController.java
+++ gateway/src/main/java/com/angels/gateway/controller/PredictController.java
@@ -1,5 +1,6 @@
 package com.angels.gateway.controller;
 
+import com.angels.gateway.domain.ParametersExample;
 import com.angels.gateway.service.PredictService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -19,25 +20,7 @@ public class PredictController {
     private final PredictService service;
 
     @PostMapping
-    @Operation(requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(content = @Content(schema = @Schema(example = "{\n" +
-            "   \"previous_weight\": 0,\n" +
-            "  \"gestational_risk\": 0,\n" +
-            "  \"schooling\": 0,\n" +
-            "  \"has_hypertension\": true,\n" +
-            "  \"has_diabetes\": true,\n" +
-            "  \"has_pelvic_sugery\": true,\n" +
-            "  \"has_urinary_infection\": true,\n" +
-            "  \"has_congenital_malformation\": true,\n" +
-            "  \"has_family_twinship\": true,\n" +
-            "  \"amount_gestation\": 0,\n" +
-            "  \"amount_abortion\": 0,\n" +
-            "  \"amount_deliveries\": 0,\n" +
-            "  \"amount_cesarean\": 0,\n" +
-            "  \"target\": 0,\n" +
-            "  \"age\": 0,\n" +
-            "  \"fist_prenatal\": 0,\n" +
-            "  \"time_between_pregnancies\": 0\n" +
-            "}"))))
+    @Operation(requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(content = @Content(schema = @Schema(implementation = ParametersExample.class))))
     @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(example = "{\n \"model-1\": {}, \n \"model-2\": {} \n }")))
     public ResponseEntity<Map<String, ?>> multiPredicts(@RequestBody Map<String, ?> parameters) {
         return ResponseEntity.ok(service.MultiPredicts(parameters));
